99c7265dea9c64c5106bc661ba515053ececfdd4
fabricio3g <fabricio.3g@gmail.com>
2021-11-15 18:21:32 -0300

    tweet component added a

--- client/src/Components/Main.jsx
+++ client/src/Components/Main.jsx
@@ -1,9 +1,9 @@
-import {BsChatQuoteFill, BsTwitter, BsFillHouseDoorFill, BsFillQuestionCircleFill} from 'react-icons/bs'
+import {BsChatQuoteFill, BsTwitter, BsFillHouseDoorFill, BsFillQuestionCircleFill, BsHeart} from 'react-icons/bs'
 
 import React,{ Component } from 'react'
 import { Container, Button, PanelLeft, PanelRight, 
   PanelMid, TwittsContainers, ButtonContainer, TwitterIcon, 
-  TopNavPanel, FormCard, TextareaForm, TextPanelLeft
+  TopNavPanel, FormCard, TextareaForm, TextPanelLeft, UserPhotoRound, TweetCard, TweetCardText
 } from '../Styles.jsx'
 
 class Main extends Component{
@@ -31,12 +31,14 @@ class Main extends Component{
             </div>
             <Button background textColor><i className="tweetIcon"> <BsChatQuoteFill/></i><TextPanelLeft tweet center>Twittear</TextPanelLeft></Button>
           </ButtonContainer>
-          <Button>
-            <TwitterIcon/>
-            <ul style={{"listStyle": "none", "fontSize": "15px"}}>
-                <li>Fabricio</li>    
-                <li><small style={{"fontWeight": "100"}}>@fabricio3g</small></li>
-            </ul>
+          <Button userBtn>
+              <UserPhotoRound>
+                <img src="https://avatars.githubusercontent.com/u/48396639?v=4" alt="" srcset="" />  
+              </UserPhotoRound>
+              <ul style={{"listStyle": "none", "fontSize": "15px"}}>
+                  <li>Fabricio</li>    
+                  <li><small style={{"fontWeight": "300"}}>@fabricio3g</small></li>
+              </ul>
           </Button>
         </PanelLeft>
         <PanelMid>
@@ -47,6 +49,15 @@ class Main extends Component{
               <TextareaForm onChange={this.taxtareaFrom} placeholder="What's happening?"></TextareaForm>
               <Button background textColor style={{"width": "5rem", "height": "2.3rem", "marginRight":"1rem", "textAlign": "center"}}><p style={{"margin": "0 auto"}}>Tweet</p></Button>
             </FormCard>
+            <TweetCard>
+              <UserPhotoRound >
+                <img src="https://avatars.githubusercontent.com/u/48396639?v=4" alt="" srcset="" />  
+                </UserPhotoRound >
+                <TweetCardText readOnly={true} >
+                  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet ipsa sed repellat provident, quia illo distinctio minima! Nemo rem, est saepe vitae eius cupiditate natus quas, placeat deleniti assumenda inventore.
+                </TweetCardText>
+                <BsHeart/>
+            </TweetCard>
           </TwittsContainers>
         </PanelMid>
         <PanelRight>
