fbe57cf06cae81cbd5f185af8beca63f237f2c89
fabricio3g <fabricio.3g@gmail.com>
2021-11-16 20:31:03 -0300

    new component

---
+++ client/src/Components/PanelLeftComponent.jsx
@@ -0,0 +1,45 @@
+import {BsChatQuoteFill, BsTwitter, BsFillHouseDoorFill, BsFillQuestionCircleFill} from 'react-icons/bs'
+import { Button, PanelLeft, ButtonContainer, TwitterIcon, TextPanelLeft, UserPhotoRound, UserInfoUl} from '../Styles.jsx'
+import React,{ Component } from 'react'
+
+
+class PanelLeftComponent extends Component{
+
+
+  render(){
+    return(
+      <PanelLeft>
+         <TwitterIcon>
+          <BsTwitter />
+         </TwitterIcon>
+        <ButtonContainer>
+          <div>
+            <Button><BsFillHouseDoorFill style={{"marginLeft": "10px"}}/> <TextPanelLeft> Home </TextPanelLeft> </Button>
+            <Button><BsFillQuestionCircleFill style={{"marginLeft": "10px"}}/><TextPanelLeft>About</TextPanelLeft></Button>
+          </div>
+          <div>
+            <Button background textColor><i className="tweetIcon"> <BsChatQuoteFill/></i><TextPanelLeft tweet center>Twittear</TextPanelLeft></Button>
+            <Button userBtn>
+              <UserPhotoRound>
+                <img src="https://avatars.githubusercontent.com/u/48396639?v=4" alt="" srcset="" />  
+              </UserPhotoRound>
+                <UserInfoUl>
+                    <li>Fabricio</li>    
+                    <li><small style={{"fontWeight": "300"}}>@fabricio3g</small></li>
+                </UserInfoUl>
+            </Button>
+          </div>
+        </ButtonContainer>
+        
+      </PanelLeft> )
+    
+  }
+    
+
+  }  
+  
+    
+
+  export default PanelLeftComponent
+
+
