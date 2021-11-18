bd97f17fa8bbd990e4468e8a06b54662db67764b
fabricio3g <fabricio.3g@gmail.com>
2021-11-17 22:18:54 -0300

    react router setup

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Home from './Home.jsx'
+import Home from './Views/Home.jsx'
+import Register from './Views/Register.jsx'
+import Singup from './Views/Singup.jsx'
+import Login from './Views/Login.jsx'
 
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 
 ReactDOM.render(
-    <Home/>,
+  <BrowserRouter>
+      <Routes>
+            <Route path='/' element={<Home/>} />
+            <Route path='/register' element={<Register/>} />
+            <Route path="/singup" element={<Singup />} />
+            <Route path="/Login" element={<Login />} />
+      </Routes>
+  </BrowserRouter>,
   document.getElementById('root')
 );
