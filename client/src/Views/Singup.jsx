0d1150c1c5d942e1dee469c9d1f6a864c59c8ed2
fabricio3g <fabricio.3g@gmail.com>
2021-11-18 15:50:50 -0300

    Update Singup.jsx

--- client/src/Views/Singup.jsx
+++ client/src/Views/Singup.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import {Container, SmallCard, ButtonContainer, ImgResponsive} from '../Styles.jsx'
+import {Container, SmallCard, ButtonContainer, ImgResponsive, InputField, ButtonRegister} from '../Styles.jsx'
 
 
 class Singup extends Component{
@@ -10,7 +10,9 @@ class Singup extends Component{
                     <ImgResponsive src="https://images.unsplash.com/photo-1446776811953-b23d57bd21aa?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=872&q=80"/>
                     <SmallCard>
                         <ButtonContainer>
-                            Singup from
+                            <InputField type="email" placeholder="correo@email.com"/>
+                             <InputField type="password" placeholder="*******"/>
+                             <ButtonRegister>Singup</ButtonRegister>
                         </ButtonContainer>
                     </SmallCard>
             </Container>
