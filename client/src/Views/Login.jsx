69ccc3896d1ec4532a325cac8311c79b3ffc701f
fabricio3g <fabricio.3g@gmail.com>
2021-11-18 15:50:19 -0300

    input form

--- client/src/Views/Login.jsx
+++ client/src/Views/Login.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import {Container, SmallCard, ButtonContainer, ImgResponsive} from '../Styles.jsx'
+import {Container, SmallCard, ButtonContainer, ImgResponsive,InputField, ButtonRegister} from '../Styles.jsx'
 
 
 class Login extends Component{
@@ -10,7 +10,9 @@ class Login extends Component{
                     <ImgResponsive src="https://images.unsplash.com/photo-1446776811953-b23d57bd21aa?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=872&q=80"/>
                     <SmallCard>
                         <ButtonContainer>
-                            Login from
+                             <InputField type="email" placeholder="correo@email.com"/>
+                             <InputField type="password" placeholder="*******"/>
+                             <ButtonRegister>Login</ButtonRegister>
                         </ButtonContainer>
                     </SmallCard>
             </Container>
