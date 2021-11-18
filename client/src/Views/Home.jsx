3fb97fa632c9236c6b72f1b43e0e4f6e867345a6
fabricio3g <fabricio.3g@gmail.com>
2021-11-17 22:19:48 -0300

    moved to the view folder

---
+++ client/src/Views/Home.jsx
@@ -0,0 +1,13 @@
+import Main from '../Components/Main.jsx'
+import React, { Component } from 'react'
+
+class Home extends Component{
+  
+    render(){
+        return(
+           <Main/>
+        )
+    }
+}
+
+export default Home
