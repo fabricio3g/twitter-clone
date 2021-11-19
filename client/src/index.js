4c294c597dca9f9ce272f3403c50eb44ad682ab8
fabricio3g <fabricio.3g@gmail.com>
2021-11-19 18:18:56 -0300

    rendering components base in the route

--- client/src/index.js
+++ client/src/index.js
@@ -5,16 +5,35 @@ import Register from './Views/Register.jsx'
 import Singup from './Views/Singup.jsx'
 import Login from './Views/Login.jsx'
 
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Redirect } from 'react-router-dom'
+
+
+function isRegisterPageTrue(){
+  
+  const pathName = window.location.pathname
+
+  if(pathName ===  '/register' || pathName ===  '/singup' || pathName ===  '/login'){
+    return false
+  }
+  else{
+    return true
+  }
+}
+
 
 
 ReactDOM.render(
   <BrowserRouter>
+
+      {
+        isRegisterPageTrue() && <Home/>
+      }
+      
       <Routes>
-            <Route path='/' element={<Home/>} />
-            <Route path='/register' element={<Register/>} />
-            <Route path="/singup" element={<Singup />} />
-            <Route path="/Login" element={<Login />} />
+
+            <Route exact path='/register' element={<Register/>}/>
+            <Route exact path="/singup" element={<Singup />} />
+            <Route exact path="/Login" element={<Login />} />
       </Routes>
   </BrowserRouter>,
   document.getElementById('root')
