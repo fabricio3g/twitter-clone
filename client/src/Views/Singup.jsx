973f027f8a5b3fbc45f5e9d69e6f0602749569b6
fabricio3g <fabricio.3g@gmail.com>
2021-11-19 18:19:49 -0300

    Update Singup.jsx

--- client/src/Views/Singup.jsx
+++ client/src/Views/Singup.jsx
@@ -10,9 +10,10 @@ class Singup extends Component{
                     <ImgResponsive src="https://images.unsplash.com/photo-1446776811953-b23d57bd21aa?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=872&q=80"/>
                     <SmallCard>
                         <ButtonContainer>
+                            <InputField type="text" placeholder="username"/>
                             <InputField type="email" placeholder="correo@email.com"/>
-                             <InputField type="password" placeholder="*******"/>
-                             <ButtonRegister>Singup</ButtonRegister>
+                            <InputField type="password" placeholder="*******"/>
+                            <ButtonRegister>Singup</ButtonRegister>
                         </ButtonContainer>
                     </SmallCard>
             </Container>
